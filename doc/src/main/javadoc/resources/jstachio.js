35e87ba02c0c820d6a6c0461da2d9b819a841b03
Adam Gent <adam.gent@snaphop.com>
2022-11-10 10:05:24 -0500

    Add automatic TOC

---
+++ doc/src/main/javadoc/resources/jstachio.js
@@ -0,0 +1,11 @@
+ tocbot.init({
+   // Where to render the table of contents.
+   tocSelector: '.js-toc',
+   // Where to grab the headings to build the table of contents.
+   contentSelector: '.js-toc-content',
+   // Which headings to grab inside of the contentSelector element.
+   headingSelector: 'h1, h2, h3',
+   // For headings inside relative or absolute positioned containers within content.
+   hasInnerContainers: true,
+   collapseDepth: 4
+ });
