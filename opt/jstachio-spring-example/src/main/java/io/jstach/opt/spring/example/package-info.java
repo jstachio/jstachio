db0122363a90ed5b39168818b56e9fde04af4264
Adam Gent <adam.gent@snaphop.com>
2022-12-03 17:47:21 -0500

    More spring doc #10

--- opt/jstachio-spring-example/src/main/java/io/jstach/opt/spring/example/package-info.java
+++ opt/jstachio-spring-example/src/main/java/io/jstach/opt/spring/example/package-info.java
@@ -1,5 +1,19 @@
 /**
- * This package is only exported for documenting the Spring Example. In a real world app
- * you probably would not export a package like this.
+ * Spring MVC application with:
+ * <ul>
+ * <li>{@link io.jstach.opt.spring.example.HelloController Controller}</li>
+ * <li>{@link io.jstach.opt.spring.example.HelloModel Model}</li>
+ * <li>{@link io.jstach.opt.spring.example.HelloModelView View (jstachio generated
+ * renderer)}</li>
+ * <li>{@link io.jstach.opt.spring.example.SpringTemplateConfig Bean configuration}</li>
+ * <li>{@link io.jstach.opt.spring.example.WebConfig Web configuration}</li>
+ * </ul>
+ * <strong> Make sure to take note of the {@link io.jstach.opt.spring.example/ annotations
+ * on this module as they define the jstachio config} needed to integrate with Spring.
+ * </strong> This is because much of JStachio config is <em>not runtime driven but compile
+ * time driven</em>.
+ * <p>
+ * <em>This package is only exported for documenting the Spring Example. In a real world
+ * app you probably would not export a package like this. </em>
  */
 package io.jstach.opt.spring.example;
