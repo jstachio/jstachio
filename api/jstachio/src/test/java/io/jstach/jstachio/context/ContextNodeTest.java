1e11792ef206e38e767cd713ee8d9ce8bf50baf9
Adam Gent <adam.gent@snaphop.com>
2023-06-01 17:20:14 -0400

    Fix #161 ContextNode.isFalsey arrays

---
+++ api/jstachio/src/test/java/io/jstach/jstachio/context/ContextNodeTest.java
@@ -0,0 +1,42 @@
+package io.jstach.jstachio.context;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+public class ContextNodeTest {
+
+	@Test
+	public void testIsFalseyArray() {
+		int[] ia = new int[] { 1, 0 };
+		assertFalse(ContextNode.isFalsey(ia));
+		assertTrue(ContextNode.ofRoot(ia).iterator().hasNext());
+		ia = new int[] {};
+		assertTrue(ContextNode.isFalsey(ia));
+		assertFalse(ContextNode.ofRoot(ia).iterator().hasNext());
+	}
+
+	@Test
+	public void testIteratorOnArray() throws Exception {
+		int[] ia = new int[] { 1, 0 };
+		var node = ContextNode.ofRoot(ia);
+		StringBuilder sb = new StringBuilder();
+		for (var n : node) {
+			sb.append(n.renderString()).append("\n");
+		}
+		String expected = """
+				1
+				0
+				""";
+		assertEquals(expected, sb.toString());
+
+	}
+
+	@Test
+	public void testEmptyMapIsNotFalsey() throws Exception {
+		assertFalse(ContextNode.isFalsey(Map.of()));
+	}
+
+}
