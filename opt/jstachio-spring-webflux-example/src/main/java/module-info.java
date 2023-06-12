aca1f41981a488d09e80b0f226e085aeac7101ca
Adam Gent <adam.gent@snaphop.com>
2023-06-12 11:27:48 -0400

    Doc example code does not follow semver

--- opt/jstachio-spring-webflux-example/src/main/java/module-info.java
+++ opt/jstachio-spring-webflux-example/src/main/java/module-info.java
@@ -21,6 +21,7 @@
  * <a href="https://github.com/jstachio/jstachio/tree/main/opt/jstachio-spring-webflux-example">directly on github.</a> 
  * </em>
  * 
+ * @apiNote This module is not public API as it is just an example and thus does not follow semver policy!
  * @author agentgt
  */
 module io.jstach.opt.spring.webflux.example {
