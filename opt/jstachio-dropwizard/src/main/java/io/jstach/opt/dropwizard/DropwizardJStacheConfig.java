31f8072f79579e2facbabd0bfa901db0ed6d8fc0
Adam Gent <adam.gent@snaphop.com>
2023-07-14 15:23:40 -0400

    Improve dropwizard doc #188

--- opt/jstachio-dropwizard/src/main/java/io/jstach/opt/dropwizard/DropwizardJStacheConfig.java
+++ opt/jstachio-dropwizard/src/main/java/io/jstach/opt/dropwizard/DropwizardJStacheConfig.java
@@ -8,12 +8,15 @@ import io.jstach.jstache.JStachePath;
  * Opinionated static config for dropwizard based on how dropwizard stores templates for
  * Mustache.java. Place the below on a <code>package-info.java</code> where your JStache
  * models are or if in a modular environment <code>module-info.java</code>
+ *
  * <pre><code class="language-java">
  * &#64;JStacheConfig(using = DropwizardJStacheConfig.class)
  * &#47;&#47; some class, package-info, module-info
- * </code> </pre> Assuming you have a JStache model with class name <code>MyView</code> in
- * package <code>com.company.model</code> The template location if not specified will
- * default to: <code>src/main/resources/com/company/model/MyModel.mustache</code>.
+ * </code>
+ *
+ * </pre> Assuming you have a JStache model with class name <code>MyView</code> in package
+ * <code>com.company.model</code> The template location if not specified will default to:
+ * <code>src/main/resources/com/company/model/MyModel.mustache</code>.
  * <p>
  * This configuration will also enforce and check that your models <code>implements</code>
  * {@link JStacheViewSupport} which provides {@link JStacheViewSupport#toView()}.
--- opt/jstachio-dropwizard/src/main/java/module-info.java
+++ opt/jstachio-dropwizard/src/main/java/module-info.java
@@ -3,8 +3,30 @@ import io.jstach.opt.dropwizard.JStachioViewRenderer;
 /**
  * JStachio Dropwizard integration:
  * <a href="https://repo1.maven.org/maven2/io/jstach/jstachio-dropwizard/maven-metadata.xml" class="gav">io.jstach:jstachio-dropwizard</a>.
+ * <p>
+ * <strong>Step 1: add ViewBundle</strong>
  * <pre><code class="language-java">
  * bootstrap.addBundle(new ViewBundle&lt;&gt;());
+ * </code>
+ * </pre>
+ * <strong>Step 2: annotate package-info or module-info</strong>
+ * <pre><code class="language-java">
+ * &#64;JStacheConfig(using = DropwizardJStacheConfig.class)
+ * &#47;&#47; some class, package-info, module-info
+ * </code>
+ * </pre>
+ * <strong>Step 3: have JStache models implement {@link io.jstach.opt.dropwizard.JStacheViewSupport}</strong>
+ * <pre><code class="language-java">
+ * &#64;JStache
+ * public record ExampleModel(String message) implements JStacheViewSupport {}
+ * </code>
+ * </pre>
+ * <strong>Step 4: return dropwizard views by using toView()</strong>
+ * <pre><code class="language-java">
+ * &#64;GET
+ * public View hello() {
+ *     return new ExampleModel("Hello world dropwizard using mixin").toView();
+ * }
  * </code>
  * </pre>
  * @author agentgt
