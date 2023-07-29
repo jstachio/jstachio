2b93b6c54726c06c607b3d856d907ab22fd0af7a
Adam Gent <adam.gent@snaphop.com>
2023-07-29 18:07:03 -0400

    Fix test #200

--- compiler/apt/src/test/java/io/jstach/apt/IncrementalGradleTest.java
+++ compiler/apt/src/test/java/io/jstach/apt/IncrementalGradleTest.java
@@ -1,14 +1,25 @@
 package io.jstach.apt;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
 
 import org.junit.Test;
 
 public class IncrementalGradleTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws IOException {
+		var is = GenerateRendererProcessor.class
+				.getResourceAsStream("/META-INF/gradle/incremental.annotation.processors");
+		InputStreamReader r = new InputStreamReader(is, StandardCharsets.UTF_8);
+		StringWriter sw = new StringWriter();
+		r.transferTo(sw);
+		String expected = GenerateRendererProcessor.class.getCanonicalName() + ",dynamic";
+		assertEquals(expected, sw.toString());
 	}
 
 }
