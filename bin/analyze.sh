4aca0e730cac6301685e078111dfecdaa9c896af
Adam Gent <adam.gent@snaphop.com>
2023-08-17 17:04:10 -0400

    Add eclipse null analysis

--- bin/analyze.sh
+++ bin/analyze.sh
@@ -5,7 +5,7 @@ if [ -z "$_profiles" ]; then
   _profiles="checkerframework errorprone"
 fi
 for profile in $_profiles; do
-./mvnw clean verify -pl api/jstachio -P${profile} -Dmaven.javadoc.skip -DskipTests -Dmaven.source.skip=true
+./mvnw clean verify -pl api/jstachio -P${profile} -Dmaven.javadoc.skip -DskipTests -Dmaven.source.skip=true 
 done
 
 # Checker or the maven compiler leaves these files around
