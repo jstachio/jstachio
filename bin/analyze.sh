2b9cc0ae973317053944b548d7b5f956c7a8c19e
Adam Gent <adam.gent@snaphop.com>
2023-08-21 14:44:07 -0400

    Fix return value of analyze script

--- bin/analyze.sh
+++ bin/analyze.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+_MAVEN_CLI_OPTS="--batch-mode --no-transfer-progress"
+
 _profiles="$1"
 if [ -z "$_profiles" ]; then
   _profiles="checkerframework errorprone eclipse"
 fi
 for profile in $_profiles; do
-./mvnw clean verify -pl api/jstachio,compiler/apt  -P${profile} -Dmaven.javadoc.skip -DskipTests -Dmaven.source.skip=true 
+./mvnw $_MAVEN_CLI_OPTS clean verify -pl api/jstachio,compiler/apt  -P${profile} -Dmaven.javadoc.skip -DskipTests -Dmaven.source.skip=true 
 done
 
 # Checker or the maven compiler leaves these files around
 # I'm not sure why
-find . -name "javac.*.args" | xargs rm
+find . -name "javac.*.args" | xargs rm -f
